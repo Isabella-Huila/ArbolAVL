7fb1f641268a0e32906810e03387acad3858761c
Isabella <isahc221004@gmail.com>
2023-04-26 17:48:27 -0500

    control

---
+++ src/NodoArbolAVL.java
@@ -0,0 +1,21 @@
+public class NodoArbolAVL {
+
+
+    int dato;
+    int fe;
+
+    NodoArbolAVL hijoIzquierdo,hijoDerecho;
+
+    public NodoArbolAVL(int d) {
+        this.dato = d;
+        this.fe = 0;
+        this.hijoIzquierdo = null;
+        this.hijoDerecho = null;
+
+
+
+
+    }
+
+
+}
